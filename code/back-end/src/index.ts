f357f3de54afb2241452f14dc1d22e045318ae2c
Carlos Garcia <chenzhen.dev@gmail.com>
2023-04-22 17:09:59 -0300

    feat(server): levantando Express framework

---
+++ code/back-end/src/index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response, NextFunction } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app:Express = express()
+
+const port = process.env.PORT || 8000
+
+app.listen(port,() => {
+    console.log(`Server is running at http://localhost:${port}`)
+})

f89a78e8287ad80cf43f074a1d65cccc5eda7533
Carlos Garcia <chenzhen.dev@gmail.com>
2023-04-25 18:41:17 -0300

    feat(database): levantando banco de dados no bootstrap

--- code/back-end/src/main/server.ts
+++ code/back-end/src/main/server.ts
@@ -1,9 +1,17 @@
-import dotenv from 'dotenv'
-import { app } from './config/app'
+import dotenv from "dotenv";
+import { app } from "./config/app";
+import { MongooseDatabase } from "../infra/database/MongooseDatabase";
 
-dotenv.config()
-const port = process.env.PORT || 8000
+dotenv.config();
+const port = process.env.PORT || 8000;
+const db = new MongooseDatabase();
 
-app.listen(port,() => {
-    console.log(`Server is running at http://localhost:${port}`)
-})
+db.start()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running at http://localhost:${port}`);
+        });
+    })
+    .catch((error) => {
+        console.log(`Erro ao conectar ao banco de dados: ${error}`)
+    });

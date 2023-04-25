53a7ae61b36b7d8cfb6520fa44841daabb6a343f
Carlos Garcia <chenzhen.dev@gmail.com>
2023-04-25 16:56:40 -0300

    feat(rotas): setup de rotas

--- code/back-end/src/main/config/routes.ts
+++ code/back-end/src/main/config/routes.ts
@@ -1,3 +1,12 @@
-import { Express } from "express";
+import { Express, Router } from "express";
+import fg from "fast-glob";
+import path from "path";
+import fs from "fs/promises";
 
-export default (app: Express): void => {};
+export default async (app: Express): Promise<void> => {
+    const router = Router();
+    const setRoute = async () => (await import(`../routes/exercicios-routes`)).default(router); //  Precisa ser refatorado
+    
+    app.use("/api", router);
+    setRoute();
+};

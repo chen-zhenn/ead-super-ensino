f56b8eda19a8060868c0b14ee7902f59d4729bac
Carlos Garcia <chenzhen.dev@gmail.com>
2023-04-25 01:44:27 -0300

    feat(controller): implementação de ExerciseController

--- code/back-end/src/presentation/controllers/ExerciseController.ts
+++ code/back-end/src/presentation/controllers/ExerciseController.ts
@@ -1,8 +1,10 @@
 import { IExercise } from "../../domain/IExercise"
 import { IExerciseController } from "../../domain/IExerciseController"
+import { IExerciseRepository } from "../../domain/IExerciseRepository"
 
 export default class ExerciseController implements IExerciseController {
-    list():IExercise[]{
+    constructor(private exerciseRepository: IExerciseRepository) {}
+    async list():Promise<IExercise[]>{
         return []
     }
 }

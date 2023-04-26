e2df1cbe481aec49ef873d6157a3785d74c9cf10
Carlos Garcia <chenzhen.dev@gmail.com>
2023-04-26 04:44:35 -0300

    feat(server): trocando instancia do database em server

--- code/back-end/src/main/server.ts
+++ code/back-end/src/main/server.ts
@@ -1,17 +1,17 @@
 import dotenv from "dotenv";
 import { app } from "./config/app";
-import { MongooseDatabase } from "../infra/database/MongooseDatabase";
+import { MongoDatabase } from "../infra/database/mongodb/MongoDatabase";
 
 dotenv.config();
 const port = process.env.PORT || 8000;
-const db = new MongooseDatabase();
+const dbMongo = MongoDatabase.getInstance();
 
-db.start()
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`Server is running at http://localhost:${port}`);
-        });
-    })
-    .catch((error) => {
-        console.log(`Erro ao conectar ao banco de dados: ${error}`)
+dbMongo.connect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
     });
+  })
+  .catch((error) => {
+    console.log(`Erro ao conectar ao banco de dados: ${error}`)
+  });

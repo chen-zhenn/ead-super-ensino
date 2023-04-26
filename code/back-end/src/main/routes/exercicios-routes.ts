0a580cd6c154f26501af90bd4d05908b5fa13f5a
Carlos Garcia <chenzhen.dev@gmail.com>
2023-04-26 04:56:31 -0300

    feat(rota): adptação da rota de exercicios

--- code/back-end/src/main/routes/exercicios-routes.ts
+++ code/back-end/src/main/routes/exercicios-routes.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
+import { makeExerciseController as exerciseController } from "../../main/factories/ExerciseFactory";
 
 export default (router: Router): void => {
-    router.get("/exercicios", (req, res) => {
-        res.status(200).json({exercicios: "..."})
-    })
+  
+  router.get("/exercicios", async (req, res) => {
+    try {
+      const exercises = await exerciseController();
+      const list = await exercises.list();
+      res.status(200).send(list);
+    } catch (error) {
+      res.status(500).send({ error: "Internal server error" });
+    }
+  });
 }

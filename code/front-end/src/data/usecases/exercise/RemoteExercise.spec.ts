472d853549323afbaf79fedec4d814257298cea3
Carlos Garcia <chenzhen.dev@gmail.com>
2023-04-26 12:56:53 -0300

    test(exercise): iniciando implementação concreta(classe) para caso de uso de exercícios

---
+++ code/front-end/src/data/usecases/exercise/RemoteExercise.spec.ts
@@ -0,0 +1,23 @@
+import { IHttpGetClient } from "data/protocols/http/IHttpCliente";
+import { RemoteExercise } from "./RemoteExercise";
+
+describe("RemoteExercise", () => {
+  it("Deve ser chamado com URL correta", async () => {
+    class HttpGetClientSpy {
+        public url?: string
+        
+        constructor(url: string){
+            this.url = url
+        }
+
+        async get(url: string): Promise<void> {
+            return Promise.resolve();
+        }
+    }
+    const url = "some_url";
+    const httGetClient = new HttpGetClientSpy(url);
+    const sut = new RemoteExercise(url, httGetClient);
+    await sut.list();
+    expect(httGetClient.url).toBe(url)
+  });
+});

50eb3b18d4335cbd9ce93d43c3e8a2cf8d0e431b
Carlos Garcia <chenzhen.dev@gmail.com>
2023-04-27 01:55:46 -0300

    feat(mapping-state): mapeando dados de estado global em pagina e rota de exercício

--- code/front-end/src/pages/Disciplinas.tsx
+++ code/front-end/src/pages/Disciplinas.tsx
@@ -1,18 +1,24 @@
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Context from "@/presentation/contexts/exercicios";
-import { IExercise } from "@/domain/models/IExercise";
 
 export default function Disciplinas() {
   const { exercicios } = useContext(Context);
+  const [disciplina, setDisciplina] = useState<Set<string>>(new Set());
+
+  useEffect(() => {
+    const dataSet = new Set(disciplina);
+    for (let item of exercicios) setDisciplina(dataSet.add(item.disciplina))
+  }, [exercicios]);
+
   return (
     <>
       <h2>Pagina de Disciplinas</h2>
       <ul>
-        {exercicios.map((item: IExercise ) => (
-          <li>
-            <Link href={`/exercicios/${item.disciplina}`} passHref>
-              {item.disciplina}
+        {Array.from(disciplina).map((item,index) => (
+          <li key={index}>
+            <Link href={`/exercicios/${item}`} passHref>
+              {item}
             </Link>
           </li>
         ))}
--- code/front-end/src/pages/exercicios/[slug].tsx
+++ code/front-end/src/pages/exercicios/[slug].tsx
@@ -1,14 +1,28 @@
 import { useRouter } from 'next/router'
+import { useContext, useEffect, useState } from "react";
 import Context from "@/presentation/contexts/exercicios";
+import { IExercise } from '@/domain/models/IExercise';
 
 export default function () {
+    const { exercicios } = useContext(Context);
+    const [data, seData] = useState<IExercise[]>([])
     const router = useRouter()
     const { slug } = router.query
-    // const { data } = useContext(ExerciciosContext);
+
+    useEffect(() => {
+        seData(exercicios.filter(item => item.disciplina === slug))
+    },[exercicios])
     
     return (
         <>
             <h2>Exercício - {slug}</h2>
+            <ul>
+                {data.map((item, index) => (
+                    <li key={index}>
+                        {item.tema}
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }

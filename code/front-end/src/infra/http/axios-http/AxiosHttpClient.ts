d9417a113b70759ad0f9763309659cd5bc522096
Carlos Garcia <chenzhen.dev@gmail.com>
2023-04-26 23:40:54 -0300

    fix(http-client): adaptando o tipo de retorno para Axios http client

--- code/front-end/src/infra/http/axios-http/AxiosHttpClient.ts
+++ code/front-end/src/infra/http/axios-http/AxiosHttpClient.ts
@@ -1,7 +1,8 @@
+import { IHttpResponse } from "@/data/protocols/http/IHttpResponse";
 import axios from "axios";
 
 export class AxiosHttpClient {
-  static async get(url: string): Promise<void> {
+  static async get(url: string): Promise<IHttpResponse> {
     return await axios.get(url);
   }
 }

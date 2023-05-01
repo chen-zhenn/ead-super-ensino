65d92cd309226771ac2f24052bd65de288334190
Carlos Garcia <chenzhen.dev@gmail.com>
2023-04-30 21:56:34 -0300

    refactor(build): aplicando SSG

--- code/front-end/src/pages/index.tsx
+++ code/front-end/src/pages/index.tsx
@@ -1,10 +1,20 @@
 import Head from "next/head";
-import Image from "next/image";
-import Disciplinas from "./Disciplinas";
-
+import { memo } from "react";
+import { makeExercise as exercise } from "@/main/factories/exercises/ExerciseFactory";
+import { IExercise } from "@/domain/models/IExercise";
+import DisciplinasList from "@/presentation/components/list/disciplinas";
 import styles from "@/pages/index.module.css";
 
-export default function Home() {
+interface IExerciseData {
+  _id: string;
+  exercicios: IExercise[];
+}
+
+interface Props {
+  exercises: IExerciseData;
+}
+
+function Home({exercises}:Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,10 +25,33 @@ export default function Home() {
       <main style={{
         justifyContent:'flex-start', 
         marginRight:'auto', paddingTop:'0.625rem'}}>
-        <Disciplinas />
+        <DisciplinasList exercises={exercises}/>
       </main>
 
       <footer className={styles.footer}></footer>
     </div>
   );
+}
+
+export default memo(Home)
+
+export async function getStaticProps() {
+
+  const exercises:IExerciseData = {
+    exercicios: [],
+    _id: "",
+  }
+  
+  await exercise()
+    .getData()
+    .then((response) => {
+      const [bodyData] = response.body;
+      const { _id, exercicios } = bodyData
+      exercises._id = _id
+      exercises.exercicios = exercicios
+  });
+
+  return {
+    props: { exercises }
+  }
 }
---
+++ code/front-end/src/presentation/components/list/disciplinas/index.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import { Card } from "antd";
+import { memo, useContext, useEffect, useState } from "react";
+import { IExercise } from "@/domain/models/IExercise";
+import { ExeciseContext } from "@/presentation/contexts/exercicios";
+
+interface IExerciseData {
+  _id: string;
+  exercicios: IExercise[];
+}
+
+interface Props {
+  exercises: IExerciseData;
+}
+
+function Disciplinas({ exercises }: Props) {
+  const { exercicios } = useContext(ExeciseContext);
+  const [disciplinas, setDisciplinas] = useState<Set<string>>(new Set());
+  const [data, setData] = useState<IExerciseData>(exercises);
+
+  useEffect(() => {
+    setData(exercises);
+    setDataDisciplinaData(data.exercicios);
+  }, []);
+
+  useEffect(() => {
+    setDataDisciplinaData(exercicios);
+  }, [exercicios]);
+
+  function setDataDisciplinaData(list: IExercise[]) {
+    const dataSet = new Set(disciplinas);
+    for (let item of list) setDisciplinas(dataSet.add(item.disciplina));
+  }
+
+  return (
+    <>
+      <h1
+        style={{
+          alignSelf: "flex-start",
+          marginLeft: "unset",
+          paddingLeft: "unset",
+        }}
+      >
+        Disciplinas
+      </h1>
+      <ul
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          listStyleType: "none",
+        }}
+      >
+        {Array.from(disciplinas).map((item, index) => (
+          <li key={index} style={{ marginRight: "1.5rem" }}>
+            <Link href={`/exercicios/${item}`} passHref>
+              <Card title="Exercícios" bordered={true}>
+                <h3>{item}</h3>
+              </Card>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default memo(Disciplinas);

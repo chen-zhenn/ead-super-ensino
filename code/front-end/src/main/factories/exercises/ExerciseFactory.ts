210ee9df7281afc383f79ce87d2593574b323436
Carlos Garcia <chenzhen.dev@gmail.com>
2023-05-03 16:54:14 -0300

    feat(controller): dispobibilizando um metodo para mock

--- code/front-end/src/main/factories/exercises/ExerciseFactory.ts
+++ code/front-end/src/main/factories/exercises/ExerciseFactory.ts
@@ -1,16 +1,22 @@
 import { AxiosHttpClient } from "@/infra/http/axios-http/AxiosHttpClient";
 import { RemoteExercise } from "@/data/usecases/exercise/RemoteExercise";
+import mock from  "../../../../../back-end/src/db.json"
 
 export const makeExercise = () => {
   const url = "http://localhost:8000/api/exercicios";
   const httpClient = AxiosHttpClient;
   const remoteExercise = new RemoteExercise(url, httpClient);
+
+  async function getMock() {
+    return await mock
+  }
   
   async function getData() {
     return await remoteExercise.list()
   }
 
   return {
-    getData
+    getData,
+    getMock
   }
 };
